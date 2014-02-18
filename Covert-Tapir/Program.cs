faeac6dbcfd6025df0ffb05094f447e3e90d3453
Randall Sewell <randallsewell@gmail.com>
2014-02-18 09:16:08 -0800

    Method framework defined

--- Covert-Tapir/Program.cs
+++ Covert-Tapir/Program.cs
@@ -6,10 +6,60 @@ using System.Threading.Tasks;
 
 namespace Covert_Tapir
 {
-    class Program
+    class TestConvexHull : ConvexHull
     {
+
+
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Test");
+            TestConvexHull testicle = new TestConvexHull();
+            System.Console.WriteLine( testicle.JarvisMarch );
+            System.Console.ReadLine();
         }
+    }
+
+    abstract class ConvexHull
+    {
+        protected int JarvisMarch
+        {
+            get { return 0; }
+        }
+
+        protected int GrahamScan
+        {
+            get { return 0; }
+        }
+
+        protected int QuickHull
+        {
+            get { return 0; }
+        }
+
+        protected int DivideAndConquer
+        {
+            get { return 0; }
+        }
+
+        protected int MonotoneChain
+        {
+            get { return 0; }
+        }
+
+        protected int Incremental
+        {
+            get { return 0; }
+        }
+
+        protected int ICPH
+        {
+            get { return 0; }
+        }
+
+        protected int ChansAlgorithm
+        {
+            get { return 0; }
+        }
+
     }
 }
